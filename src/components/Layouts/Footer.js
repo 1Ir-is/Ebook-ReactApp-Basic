4247813d6358b2bdf59ed565761bfe8a2a12b17d
1Ir-is <mhuynk1005@gmail.com>
2024-08-14 17:01:24 +0700

    feat: fix the header and footer

--- src/components/Layouts/Footer.js
+++ src/components/Layouts/Footer.js
@@ -1,11 +1,10 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../../assets/logo.png'
 
 export const Footer = () => {
     return (
-        <footer className="bg-white border-t max-w-screen-xl mx-auto dark:bg-gray-900 dark:border-t-1 dark:border-gray-900">
-            <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
+        <footer className="bg-white dark:bg-gray-900 dark:border-t-1 dark:border-gray-900 ">
+            <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8 border-t">
                 <div className="md:flex md:justify-between">
                     <div className="mb-6 md:mb-0">
                         <a href="/" className="flex items-center">
