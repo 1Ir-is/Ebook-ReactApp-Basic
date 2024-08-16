8f476d4555cc023b027b742e283a125eeae11177
1Ir-is <mhuynk1005@gmail.com>
2024-08-16 12:48:12 +0700

    feat: create dynamic title

--- src/pages/Home/HomePage.js
+++ src/pages/Home/HomePage.js
@@ -2,8 +2,10 @@ import { Hero } from "./components/Hero";
 import { FeaturedProduct } from "./components/FeaturedProduct";
 import { Testimonials } from "./components/Testimonials";
 import { Faq } from "./components/Faq";
+import { useTitle } from "../../hooks/useTitle";
 
 export const HomePage = () => {
+  useTitle("Access Lastest Computer Science eBooks");
   return (
     <main>
         <Hero />
--- src/pages/Products/ProductsList.js
+++ src/pages/Products/ProductsList.js
@@ -3,20 +3,21 @@ import { useLocation } from "react-router-dom";
 
 import { ProductCard } from "../../components";
 import { FilterBar } from "./components/FilterBar";
+import { useTitle } from "../../hooks/useTitle";
 
 
 export const ProductsList = () => {
+  useTitle("Explore eBooks Collection");
   const [show, setShow] = useState(false);
   const [products, setProducts] = useState([]);
   const search = useLocation().search;
   const searchTerm = new URLSearchParams(search).get("q");
-  console.log(searchTerm);
+  useTitle("Explore eBooks Collection");
 
   useEffect(() => {
     async function fetchProducts() {
       const response = await fetch(`http://localhost:8000/products?name_like=${searchTerm ? searchTerm : ""}`);
       const data = await response.json();
-      console.log(data);
       setProducts(data);
     }
 
