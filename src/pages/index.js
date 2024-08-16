f4f662109b864cbffaa195cbc885938d704a628a
1Ir-is <mhuynk1005@gmail.com>
2024-08-16 15:30:15 +0700

    feat: create login and register page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,6 @@
 export { HomePage } from './Home/HomePage';
 export { ProductsList } from './Products/ProductsList';
 export { ProductDetail } from './ProductDetail';
+
+export { Register } from './Register';
+export { Login } from './Login';
