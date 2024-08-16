93fd326638f1941431810f870cbfd2b7e66be021
1Ir-is <mhuynk1005@gmail.com>
2024-08-16 14:39:50 +0700

    feat: create all filter function option

--- src/reducers/FilterReducers.js
+++ src/reducers/FilterReducers.js
@@ -1,21 +1,33 @@
 export const filterReducer = (state, action) => {
-    const { type, payload } = action;
+    const {type, payload} = action;
 
-    switch (type) {
+    switch(type){
+
         case "PRODUCT_LIST":
             return { productList: payload.products }
+        
         case "SORT_BY":
-            return
+            return {...state, sortBy: payload.sortBy}
+        
         case "RATINGS":
-            return
+            return {...state, ratings: payload.ratings}
+        
         case "BEST_SELLER_ONLY":
-            return
+            return {...state, bestSellerOnly: payload.bestSellerOnly}
+        
         case "ONLY_IN_STOCK":
-            return
-        case "CLEAR_FILTERS":
-            return
-    
+            return {...state, onlyInStock: payload.onlyInStock}
+
+        case "CLEAR_FILTER":
+            return {
+                ...state, 
+                onlyInStock: false,
+                bestSellerOnly: false,
+                sortBy: null,
+                ratings: null
+            }
+        
         default:
-            throw new Error(`Unhandled action type: ${type}`);
+            throw new Error("No Cae Found!");
     }
 }
