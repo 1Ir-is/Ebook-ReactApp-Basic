e515ac6f60019187d620ba9901a8f4d8afa5d8d6
1Ir-is <mhuynk1005@gmail.com>
2024-08-20 13:18:07 +0700

    feat: create order success and fail page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ export { ProductsList } from './Products/ProductsList';
 export { ProductDetail } from './ProductDetail';
 
 export { CartPage } from './Cart/CartPage';
+export { OrderPage } from './Order/OrderPage';
 
 export { Register } from './Register';
 export { Login } from './Login';
--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import { HomePage, ProductsList , ProductDetail, Login, Register, CartPage } from "../pages";
+import { HomePage, ProductsList , ProductDetail, Login, Register, CartPage, OrderPage } from "../pages";
 import { ProtectedRoute } from "./ProtectedRoute";
 
 export const AllRoutes = () => {
@@ -14,6 +14,7 @@ export const AllRoutes = () => {
             <Route path="/auth/register" element={<Register />} />
 
             <Route path="/cart" element={ <ProtectedRoute><CartPage /></ProtectedRoute> } />
+            <Route path="/order-summary" element={ <ProtectedRoute><OrderPage /></ProtectedRoute> } />
         </Routes>
     </>
   )
