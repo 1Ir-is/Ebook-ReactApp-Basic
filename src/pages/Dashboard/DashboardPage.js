85b82ed52cfc4aac102f5124ce4d7f6d85bdd2df
1Ir-is <mhuynk1005@gmail.com>
2024-08-20 14:05:00 +0700

    feat: display the history order in dashboard

--- src/pages/Dashboard/DashboardPage.js
+++ src/pages/Dashboard/DashboardPage.js
@@ -1,10 +1,27 @@
-import React from 'react'
-import { DashboardCard } from './components/DashboardCard'
-import { DashboardEmpty } from './components/DashboardEmpty'
-
+import { useState, useEffect } from "react";
+import { DashboardCard } from './components/DashboardCard';
+import { DashboardEmpty } from './components/DashboardEmpty';
 
 export const DashboardPage = () => {
-    const orders = [];
+
+    const [orders, setOrders] = useState([]);   
+    const token = JSON.parse(sessionStorage.getItem("token"));
+    const cbid = JSON.parse(sessionStorage.getItem("cbid"));
+
+    useEffect(() => {
+        async function fetchOrders() {
+            const response = await fetch(`http://localhost:8000/660/orders?user.id=${cbid}`, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json", Authorization: `Bearer ${token}`,
+                }
+            });
+            const data = await response.json();
+            setOrders(data);
+        }
+        fetchOrders();
+    }, []);
+
     return (
         <main>
             <section>
@@ -12,13 +29,13 @@ export const DashboardPage = () => {
             </section>
 
             <section>
-                {orders.length && orders.map((order) => (
-                    <DashboardCard key={order.id} order={order} />
-                ))}
-            </section>
-
-            <section>
-                {!orders.length && <DashboardEmpty />}
+                {orders.length > 0 ? (
+                    orders.map((order) => (
+                        <DashboardCard key={order.id} order={order} />
+                    ))
+                ) : (
+                    <DashboardEmpty />
+                )}
             </section>
         </main>
     )
