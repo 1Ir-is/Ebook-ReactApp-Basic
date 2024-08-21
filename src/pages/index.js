c454b8e181b003d6cfe1bb1559f7176a8cb0ec29
1Ir-is <mhuynk1005@gmail.com>
2024-08-21 15:17:16 +0700

    sth

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,6 @@ export { DashboardPage } from './Dashboard/DashboardPage';
 export { ProductsList } from './Products/ProductsList';
 export { ProductDetail } from './ProductDetail';
 
+export { PageNotFound } from './PageNotFound';
 export { Register } from './Register';
 export { Login } from './Login';
-export { PageNotFound } from './PageNotFound';
